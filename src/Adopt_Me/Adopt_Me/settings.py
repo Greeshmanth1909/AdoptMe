5d3124b1126cc1543f07932123bcefec7cf9f4e7
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2023-05-09 11:56:11 +0530

    same as before

--- src/Adopt_Me/Adopt_Me/settings.py
+++ src/Adopt_Me/Adopt_Me/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# redirect user to root after login
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/" 
