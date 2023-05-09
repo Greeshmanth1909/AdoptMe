f941dbd52de9cee44d7764aa5697553fda6dcbff
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2023-05-09 10:50:26 +0530

    changed title to adopt me

--- src/Adopt_Me/members/urls.py
+++ src/Adopt_Me/members/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # ... other URLs ...
 
     # Login and logout views
-    path('login/', views.login_view),
+    # path('login/', views.login_view),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
