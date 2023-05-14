665c3a478e832bb4bbf454d9c7ab766117b3622f
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2023-05-14 14:51:13 +0530

    upload form working successfully ;)

--- src/Adopt_Me/upload/views.py
+++ src/Adopt_Me/upload/views.py
@@ -1,23 +1,15 @@
 from django.shortcuts import render, redirect
-from . import forms, models
-from django.contrib import messages
+from .forms import upload_img_form
 
 # Create your views here.
 
 
 def upload_view(request, *args, **kwargs):
-    if request.method == "POST":
-        form = forms.upload_form(request.POST, request.FILES)
-        if form.is_valid():
-            animal = form.cleaned_data['animal']
-            image = form.cleaned_data['image']
-            description = form.cleaned_data['description']
+    context = dict()
+    form = upload_img_form(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return render(request, 'upload/success.html', {})
 
-            user_img =  models.upload_img(animal=animal, image=image, description=description)
-            user_img.save()
-            messages.success(request, 'upload successful')
-            return redirect('upload/success.html')
-    else:
-        form = forms.upload_form()
-        return render(request, 'upload/upload.html', {'form': form})
-    
+    context['form'] = form
+    return render(request, 'upload/upload.html', context)
