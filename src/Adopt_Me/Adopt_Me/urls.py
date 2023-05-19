658c64350f55c5741bc02bb21a1ddef5b8a34ffd
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2023-05-19 23:44:41 +0530

    added http urls for chat functionalty

--- src/Adopt_Me/Adopt_Me/urls.py
+++ src/Adopt_Me/Adopt_Me/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('', include('upload.urls')),
     path('', include('data.urls')),
     path('', include('myPosts.urls')),
+    path('', include('chat.urls')),
 ]
--- src/Adopt_Me/chat/views.py
+++ src/Adopt_Me/chat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def chat_view(request, *args, **kwargs):
+    # chat stuff here
+    context = dict()
+    
+    return render(request, 'chat/chatHome.html', context)
