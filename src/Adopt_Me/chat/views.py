821f13faa72a7aea7be69002120fbfa26cd384a6
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2023-05-25 19:50:40 +0530

    same as bef

--- src/Adopt_Me/chat/views.py
+++ src/Adopt_Me/chat/views.py
@@ -10,6 +10,9 @@ from datetime import date, datetime
 def chat_view(request, *args, **kwargs):
     # chat stuff here
     context = dict()
+
+    # get list of all chats of user here
+    recent_contacts = DirectMessages.objects.filter(sender=request.user) | DirectMessages.objects.filter(receiver=request.user)
     
     return render(request, 'chat/chatHome.html', context)
 
@@ -18,15 +21,18 @@ def chat_view(request, *args, **kwargs):
 
 def dm_view(request, username):
 
-
     # get existing chats from db, if any.
     sender = request.user
     receiver = User.objects.get(username=username)
-    old_messages = (DirectMessages.objects.filter(sender=sender) | DirectMessages.objects.filter(receiver=receiver)).order_by('date', 'time')
+    print(receiver)
+    print(sender)
+    old_messages = (DirectMessages.objects.filter(sender=sender, receiver=receiver) | DirectMessages.objects.filter(sender=receiver, receiver=sender)).order_by('date', 'time')
     context = dict()
     context['sender'] = sender
     context['receiver'] = receiver
     context['old_messages'] = old_messages
+    context['user'] = request.user
+    print(old_messages)
     # enable text field and send button
     if request.method == 'GET':
         form = SendMessage()
@@ -41,4 +47,5 @@ def dm_view(request, username):
             form.instance.time = datetime.now()
             context['form'] = form
             form.save()
-            return render(request, 'chat/dm.html', context)
+            redirect_path = f'/chats/{username}_chat'
+            return redirect(redirect_path)
