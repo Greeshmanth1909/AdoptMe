c8fa76d8c53541178f642a37234e22f92177e651
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2023-05-25 19:50:28 +0530

    http based chat functioning nominally

--- src/Adopt_Me/chat/urls.py
+++ src/Adopt_Me/chat/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import chat_view, dm_view
 urlpatterns = [
     path('chats/', chat_view),
-    path('chats/<path:username>_chat/', dm_view),
+    path('chats/<path:username>_chat/', dm_view, name='dm'),
 ]
