700dc4c6bfc38f711cc154fbac74a11f150f02b7
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2023-06-01 18:36:15 +0530

    chats connect to common ws with chat id

--- src/Adopt_Me/chat/routing.py
+++ src/Adopt_Me/chat/routing.py
@@ -4,5 +4,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chats/(?P<username>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chats/(?P<id1>\w+)_(?P<id2>\w+)/$", consumers.ChatConsumer.as_asgi()),
 ]
