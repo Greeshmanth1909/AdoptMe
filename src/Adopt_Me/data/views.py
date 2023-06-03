546ac26381a5de55519f6282190532323ed80dd3
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2023-06-03 17:25:47 +0530

    some styling

--- src/Adopt_Me/data/views.py
+++ src/Adopt_Me/data/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from .access import db_objects
+# from .access import db_objects
 from django.contrib.auth.decorators import login_required
+from upload.models import upload_img
 # Create your views here.
 
 @login_required
 def feed_view(request, *args, **kwargs):
+    db_objects = upload_img.objects.all()
     context = dict()
     context['data'] = db_objects
     context['user'] = request.user
+    print(db_objects)
     return render(request, 'post/display.html', context)
--- src/Adopt_Me/upload/forms.py
+++ src/Adopt_Me/upload/forms.py
@@ -1,11 +1,18 @@
 """create a django form that enables the user to upload image and description of rescue animal"""
 
-from django.forms import ModelForm
+from django import forms
 from .models import upload_img
 
-class upload_img_form(ModelForm):
+class upload_img_form(forms.ModelForm):
     class Meta:
         model = upload_img
         exclude = ['user', 'dateTime', 'adopted']
+        widgets = {
+            'animal': forms.TextInput(attrs={'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+
+        }
 
 
