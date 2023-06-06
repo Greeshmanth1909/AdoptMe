95d6748c2bf9fe8f1dfa8e108092101ac5cc202a
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2023-06-06 22:42:41 +0530

    registration page updates

--- src/Adopt_Me/members/views.py
+++ src/Adopt_Me/members/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from .forms import BootstrapAuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 # Create your views here.
@@ -10,12 +13,19 @@ def login_view(request, *args, **kwargs):
     return render(request, 'registration/login.html', {})
 
 
-def registration_view(request, *args, **kwargs):
-
+def registration_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
-    form = UserCreationForm()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Registration successful. You are now logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Registration failed. Please correct the errors.')
+    else:
+        form = UserCreationForm()
     return render(request, 'registration/registration.html', {'form': form})
