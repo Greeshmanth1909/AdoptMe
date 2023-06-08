fee892afd88b1329ae069b194095e510684e8a29
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2023-06-09 00:19:04 +0530

    added login path

--- src/Adopt_Me/members/urls.py
+++ src/Adopt_Me/members/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     # path('login/', views.login_view),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('registration/', views.registration_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
